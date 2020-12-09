ef5669f37f06aad922024e113c622e28c4ab8f8a
Nathan Castaldi <nathan@castaldifamily.com>
2020-12-09 09:19:26 -0500

    added v1 image spinner, and uploaded logo sheets

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ function getRndInteger(min, max) {
 
 function rollDice() {
   /* VARIABLE DECLATRATIONS */
-  
-  const stream_options = ["Netflix", "Amazon Prime Video", "Disney+", "HBO", "Hulu", "Vudu", "Crunchyroll", "CBS All Access", "Apple TV+"];
-  
+  const stream_options = [
+    "Netflix",
+    "Amazon Prime",
+    "Disney+",
+    "HBO",
+    "Hulu",
+    "Vudu",
+    "Crunchyroll",
+    "CBS All Access",
+    "Apple TV+",
+  ];
+
   const logo = ["images/netflix.png", "images/prime.png", "images/disney.jpg"];
 
   const type = ["movie", "tv show"];
@@ -28,11 +37,24 @@ function rollDice() {
     "Sports",
     "Thrillers",
   ];
-  
+
   var rank = -1;
   var typeSelection = -1;
   var categorySelection = -1;
   var randNum = -1;
+
+  /* Spin logo wheel 
+  for (var i = 0; i < 200; i++) {
+    for (var j = 0; j < 50; j++) {
+      for (var k = 0; k < 1; k++) {
+        randNum = getRndInteger(0, logo.length);
+        document.getElementById("logo_target").src = logo[randNum];
+        document.getElementById("logo_target").innerHTML = logo[randNum];
+        console.log(randNum);
+      }
+    }
+  }
+*/
 
   /* Select service to stream from */
   randNum = getRndInteger(0, stream_options.length);
@@ -40,11 +62,11 @@ function rollDice() {
   console.log(serviceSelection);
 
   /* Display service logo */
-  document.getElementById("logo_target").src = (logo[randNum]);
+  document.getElementById("logo_target").src = logo[randNum];
 
   /* Select type of media to watch */
-  randNum = getRndInteger(0, type.length);
-  typeSelection = type[randNum];
+  /* randNum = getRndInteger(0, type.length); */
+  typeSelection = type[getRndInteger(0, type.length)];
   console.log(typeSelection);
 
   /* Select media category */
@@ -55,27 +77,39 @@ function rollDice() {
   /* Select list item rank */
   randNum = getRndInteger(1, 10);
   rank = randNum;
-  console.log(rank);  
+  console.log(rank);
 
   /* Convert rank from numbers to words */
   switch (rank) {
-    case "1":
+    case 1:
       rank = "first";
       break;
-    case "2":
+    case 2:
       rank = "second";
       break;
-    case "3":
+    case 3:
       rank = "third";
       break;
-    case "4":
+    case 4:
       rank = "fourth";
       break;
-    case "5":
+    case 5:
       rank = "fifth";
       break;
-    case "6":
+    case 6:
       rank = "sixth";
+      break;
+    case 7:
+      rank = "seventh";
+      break;
+    case 8:
+      rank = "eighth";
+      break;
+    case 9:
+      rank = "ninth";
+      break;
+    case 10:
+      rank = "tenth";
       break;
   }
 
